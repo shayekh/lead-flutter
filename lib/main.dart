96d0d19a68f0ad042ba7fa3f883036f657ef401b
shayekh <shayekhebnemizan@gmail.com>
2024-01-17 02:22:19 +0600

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,135 @@
 import 'package:flutter/material.dart';
 //shayekh ebne mizan -prom - shaku.. proxy
 
-const url = 'https://th.bing.com/th/id/OIP.lELm1ifdvkgG9H0ZTL1tRQHaFj?w=203&h=180&c=7&r=0&o=5&pid=1.7';
+const url =
+    'https://th.bing.com/th/id/OIP.lELm1ifdvkgG9H0ZTL1tRQHaFj?w=203&h=180&c=7&r=0&o=5&pid=1.7';
+
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false,
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Flutter Layout Demo'),
       ),
       body: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Image.network(url, width: double.infinity, height: 250, fit: BoxFit.cover,),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Icon(Icons.camera, size: 50,),
-              Icon(Icons.share, size: 50,),
-            ],
+          Image.network(
+            url,
+            width: double.infinity,
+            height: 250,
+            fit: BoxFit.cover,
           ),
-          RichText(
-            text: TextSpan(
-            text: 'Hello',
-            style: TextStyle(
-                fontSize: 35.0,
-                color: Colors.blue,
-                fontStyle: FontStyle.italic),
-            children: [
-              TextSpan(
-                text: 'World',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 35.0,
-                    color: Colors.amber,
-                    fontStyle: FontStyle.normal),
-              )
-            ]),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  // mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Rangamati Tourist Spot',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Rangamati, Bangladesh',
+                      style: TextStyle(fontSize: 20, color: Colors.black26),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.orange,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '41',
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
-          Text('Hello Flutter', style: TextStyle(fontSize: 35.0),)
+
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(
+                      Icons.call,
+                      size: 30,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('CALL', style: TextStyle(
+                      color: Colors.deepPurpleAccent,
+                    ),)
+                  ],
+                ),
+
+                Column(
+                  children: [
+                    Icon(
+                      Icons.navigation,
+                      size: 30,
+                      color: Colors.deepPurpleAccent,
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('ROUTE', style: TextStyle(
+                        color: Colors.deepPurpleAccent
+                    ),)
+                  ],
+                ),
+
+                Column(
+                  children: [
+                    Icon(
+                      Icons.share,
+                      size: 30,
+                      color: Colors.deepPurpleAccent,
+                    ),
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('SHARE', style: TextStyle(
+                        color: Colors.deepPurpleAccent
+                    ),)
+                  ],
+                ),
+
+
+
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+          )
         ],
       ),
     ),
